3a4dfa90e1b7f78fc6622747b345abc6c9de0e0f
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-04-10 12:26:01 +0100

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import session from 'express-session';
+import passport from 'passport';
+import './config/passport';
 import dotenv from "dotenv";
+import registerRouter from './route/register';
 
+import loginRouter from './route/login';
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
-app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'ydhdhdjeje',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//routes
+app.use("/register", registerRouter);
+app.use("/login", loginRouter)
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
